f222f5e452aebd0324f00a356f2269ae73a0b446
surendra4u <surendra4u@gmail.com>
2017-08-18 18:00:35 +0530

    changed for code optimization

--- server.js
+++ server.js
@@ -5,6 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one': {
+		title: 'Article One | Surendra',
+		heading: 'Article One',
+		date: '31 July, 2017',
+		content:
+			'<p> This is the content for my first Article.  This is the content for my first Article. </p> <p> This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article.</p><p>This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. This is the content for my first Article. .....</p>'
+	},
+	'article-two': {
+			title: 'Article Two | Narendra',
+			heading: 'Article Two',
+			date: '01 Aug, 2017',
+			content:
+				'<p> This is the content for my second Article. This is the content for my second Article.</p>'
+	},
+	'article-three': {
+			title: 'Article Three | Verandra',
+			heading: 'Article Three',
+			date: '17 Aug, 2017',
+			content:
+				'<p> This is the content for my third Article.  </p>'
+
+	}
+};
+
+function createTemplate(data){
+	var title = data.title;
+	var headding = data.headding;
+	var date = data.date;
+	var content = data.content;
+	var htmlTemplate = `
+	<HTML>
+	<HEAD>
+	<TITLE> ${title} </TITLE>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	 <link href="/ui/style.css" rel="stylesheet" />
+	</HEAD>
+	<BODY>
+		<div class="container" >
+			<div>
+				<a href="/">Home</a>
+			</div>
+			<hr>
+			<h3>${headding}</h3>
+			<div>
+				${date}
+			</div>
+			<div>
+				${content}
+			</div>
+		</div>
+	</BODY>
+	</HTML>
+	`;
+	return htmlTemplate;
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,6 +74,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/:articalName', function (req, res) {
+	var articalName=req.params.articalName;
+  res.send(createTemplate(articles[articalName]));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
